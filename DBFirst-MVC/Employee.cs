8c8ae698254a4dda6bdca4dc6120c86cf3913ff9
Jagadish1824 <131241718+Jagadish1824@users.noreply.github.com>
2025-08-05 18:59:31 +0530

    Add files via upload

---
+++ DBFirst-MVC/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DBFirstPractice.Models;
+
+public partial class Employee
+{
+    public int EmployeeId { get; set; }
+
+    public string FirstName { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
+
+    public string? Email { get; set; }
+}
---
+++ DBFirst-MVC/PracticeContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace DBFirstPractice.Models;
+
+public partial class PracticeContext : DbContext
+{
+    public PracticeContext()
+    {
+    }
+
+    public PracticeContext(DbContextOptions<PracticeContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Employee> Employees { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("data source=JAGADISH;database=practice;integrated security=true;trustservercertificate=true;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Employee>(entity =>
+        {
+            entity.HasKey(e => e.EmployeeId).HasName("PK__Employee__7AD04FF1B4E8B486");
+
+            entity.ToTable("Employee");
+
+            entity.HasIndex(e => e.Email, "UQ__Employee__A9D10534954ABC90").IsUnique();
+
+            entity.Property(e => e.EmployeeId)
+                .ValueGeneratedNever()
+                .HasColumnName("EmployeeID");
+            entity.Property(e => e.Email).HasMaxLength(100);
+            entity.Property(e => e.FirstName).HasMaxLength(50);
+            entity.Property(e => e.LastName).HasMaxLength(50);
+        });
+
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
