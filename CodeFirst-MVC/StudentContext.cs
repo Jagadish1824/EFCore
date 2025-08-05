b02cbcc4827e225b753aaed4045e0ba63bf47074
Jagadish1824 <131241718+Jagadish1824@users.noreply.github.com>
2025-08-05 19:06:39 +0530

    Add files via upload

---
+++ CodeFirst-MVC/StudentContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CodeFirstPractice.Models
+{
+    public class StudentContext:DbContext
+    {
+        public DbSet<Students> Students { get; set; }
+
+        public StudentContext(DbContextOptions<StudentContext> options)
+            : base(options)
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("data source = JAGADISH; database = practice; integrated security = true; trustservercertificate = true; ");
+        }
+    }
+}
---
+++ CodeFirst-MVC/Students.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CodeFirstPractice.Models
+{
+    public class Students
+    {
+        [Key]
+        public int StuId { get; set; }
+        public string StuName { get; set; }
+        public string Marks { get; set; }
+    }
+}
